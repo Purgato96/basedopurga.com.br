71bf1fc392c8d91781c22ff3ce82e8c0047f1d29
Purgato96 <matheus_purgato@hotmail.com>
2025-10-01 16:01:29 -0300

    Fontend Completo

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@': path.resolve(__dirname, './src')
     }
   },
   server: {
