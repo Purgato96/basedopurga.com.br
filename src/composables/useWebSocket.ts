c298c7cc3909f202adf69995362e81992b115a45
Purgato96 <matheus_purgato@hotmail.com>
2025-10-23 10:52:28 -0300

    Depurando Permissions

--- src/composables/useWebSocket.ts
+++ src/composables/useWebSocket.ts
@@ -34,7 +34,7 @@ export function useWebSocket() {
     pusher.connection.bind('connected', () => {
       connectionStatus.value = 'connected';
     });
-    pusher.connection.bind('state_change', (s: any) => console.log('WS state', s));
+    pusher.connection.bind('state_change');
     pusher.connection.bind('error', (e: any) => {
       console.log('WS error', e);
       connectionStatus.value = 'error';
